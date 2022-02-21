aea16b944c20734161e13c9a1141c8d7e05f97ee
Alicia Moss <Alicia.Moss15@gmail.com>
2022-02-20 20:37:12 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <View style={styles.weatherContainer}> 
+        <ImageBackground 
+          source={require('./assets/gradient-bkgrd.png')}
+          style={styles.image}
+        />
+        <View style={styles.titles}>
+          <Text style={styles.title}>Today's Forecast</Text>
+          
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#CCD3F8',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  weatherContainer: {
+    width: '100%',
+    height: '100%',
+  },
+  titles: {
+    marginTop: '40%',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 30,
+    color: '#95A5A6',
+    fontWeight: 'bold',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    position: 'absolute',
+  }
 });
